0a875b514996ba5ced53dfff70bb278c81745a0e
Pratik khaire <khairepratik294@gmail.com>
2025-06-24 15:05:51 +0530

    Update name_age.c

--- name_age.c
+++ name_age.c
@@ -1,6 +1,16 @@
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char name[50];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", name);  
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Your name is %s and your age is %d\n", name, age);
     return 0;
 }
